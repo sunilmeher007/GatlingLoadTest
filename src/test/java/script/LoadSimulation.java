571cf61d5b6c25e914a601c53469b0789cb42afa
Sunil Meher <sunilmeher.rocky@gmail.com>
2024-03-04 08:48:02 -0500

    load test script added
    Signed-off-by: Sunil Meher <sunilmeher.rocky@gmail.com>

--- src/test/java/script/LoadSimulation.java
+++ src/test/java/script/LoadSimulation.java
@@ -11,7 +11,7 @@ import java.util.List;
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.http;
 
-public class BasicSimulation extends Simulation {
+public class LoadSimulation extends Simulation {
 
     // 1.http configuration
     private HttpProtocolBuilder protocolBuilder = http
@@ -36,12 +36,11 @@ public class BasicSimulation extends Simulation {
 
     // 2.scenario definition
     private ScenarioBuilder scn = scenario("First Test")
-            .repeat(10).on(
-                    exec(List.of(
+            .exec(List.of(
                             exec(addAnItem()),
                             exec(checkForAnItem()),
-                            exec(removeAnItem())))
-                    .pause(Duration.ofMillis(1000))
+                            exec(removeAnItem()))
+                    // .pause(Duration.ofMillis(100))
             );
 //            // .pause(2)
 //            .repeat(5000).on(
@@ -59,7 +58,16 @@ public class BasicSimulation extends Simulation {
     // 3.load simulation
     {
         setUp(
-                scn.injectOpen(atOnceUsers(1))
+                scn.injectOpen(
+                        nothingFor(5),
+                        atOnceUsers(10),
+                        // rampUsers(10).during(5), // 3
+                        constantUsersPerSec(20).during(30), // 4
+//                        constantUsersPerSec(20).during(15).randomized(), // 5
+//                        rampUsersPerSec(10).to(20).during(10), // 6
+//                        rampUsersPerSec(10).to(20).during(10).randomized(), // 7
+                        stressPeakUsers(1000).during(20)
+                )
         ).protocols(protocolBuilder);
     }
 }
